131fb4be09b7e49789ccf230009f7b23feff9292
Sardor <sardorstudent0618@gmail.com>
2024-05-19 08:31:00 +0500

    Some changes

---
+++ src/Messenger/Server/Messenger.API/Controllers/ChatUsersController.cs
@@ -0,0 +1,37 @@
+﻿using Messenger.Application.Services.ChatUser;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.API.Controllers
+{
+    [Route("api/connections")]
+    [ApiController]
+    [Authorize]
+    public class ChatUsersController : ControllerBase
+    {
+        private readonly IChatUserService _chatUserService;
+
+        public ChatUsersController(IChatUserService chatUserService)
+            => _chatUserService = chatUserService;
+
+        [HttpPost("join-chat/{chatId}")]
+        public async Task<IActionResult> PostChatUserAsync(Guid chatId)
+            => Ok();
+
+        [HttpPost("add-member")]
+        public async Task<IActionResult> PostChatUserAsync()
+            => Ok();
+
+        [HttpPost("block-chat-member")]
+        public async Task<IActionResult> UpdateChatUserAsync()
+          => Ok();
+
+        [HttpPost("block-chat/{chatId}")]
+        public async Task<IActionResult> UpdateChatAsync(Guid chatId)
+        => Ok();
+
+        [HttpDelete("leave-chat/{chatId}")]
+        public async Task<IActionResult> DeleteChatUser(Guid chatId)
+            => Ok();
+    }
+}
--- src/Messenger/Server/Messenger.API/Controllers/MessagesController.cs
+++ src/Messenger/Server/Messenger.API/Controllers/MessagesController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Messenger.Application.DataTransferObjects.Messages;
+using Messenger.Application.Models;
+using Messenger.Application.Services.Messages;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Messenger.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/messages")]
     [ApiController]
     public class MessagesController : ControllerBase
     {
+        private readonly IMessageService _messageService;
+
+        public MessagesController(IMessageService messageService)
+            => _messageService = messageService;
+
+        [HttpPost]
+        public async Task<IActionResult> PostMessageAsync(MessageCreationDTO messageCreationDTO)
+            => Ok(await _messageService.CreateMessageAsync(messageCreationDTO));
+
+        [HttpGet]
+        public IActionResult GetMessages([FromQuery] QueryParameter parameter)
+            => Ok(_messageService.RetrieveMessages(parameter));
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMessageAsync(MessageModificationDTO messageModificationDTO)
+            => Ok(await _messageService.ModifyMessageAsync(messageModificationDTO));
+
+        [HttpDelete("{messageId}")]
+        public async Task<IActionResult> DeleteAsync(Guid messageId)
+            => Ok(await _messageService.RemoveMessageAsync(messageId));
     }
 }
