db6c6131634aeba766c612c9f9e6dfff2905d47f
Sardor <sardorstudent0618@gmail.com>
2024-05-20 05:54:13 +0500

    Auth Service

---
+++ src/Messenger/Client/Messenger.UI/src/app/Services/auth.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterDTO } from '../Interfaces/Auth/register-dto';
+import { TokenDTO } from '../Interfaces/Auth/token-dto';
+import { LoginDTO } from '../Interfaces/Auth/login-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'https://localhost:7031/api/Authentication';
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: RegisterDTO) {
+    return this.http.post<TokenDTO>(`${this.apiUrl}/Register`, userData);
+  }
+
+  refreshToken(refreshTokenData: any) {
+    return this.http.post<TokenDTO>(`${this.apiUrl}/RefreshToken`, refreshTokenData);
+  }
+
+  login(loginData: LoginDTO) {
+    return this.http.post<TokenDTO>(`${this.apiUrl}/Login`, loginData);
+  }
+
+  logOut(){
+    window.localStorage.clear();
+  }
+
+  getAccessToken(){
+    return window.localStorage.getItem('accessToken');
+  }
+
+  getRefreshToken(){
+    return window.localStorage.getItem('refreshToken');
+  }
+
+  storeToken(token:TokenDTO){
+    localStorage.setItem('accessToken', token.accessToken);
+    localStorage.setItem('refreshToken', token.refreshToken);
+    localStorage.setItem('expireDate', token.expireDate.toString());
+  }
+
+  isLoggedIn():boolean{
+    return !! localStorage.getItem('accessToken');
+  }
+}
