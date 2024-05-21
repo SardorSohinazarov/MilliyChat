2ed8dd9aa407d8bbc4d81a529fcc94d6de4de769
Sardor <sardorstudent0618@gmail.com>
2024-05-21 15:12:12 +0500

    Auth Interceptor added

---
+++ src/Messenger/Client/Messenger.UI/src/app/Interceptor/auth.interceptor.ts
@@ -0,0 +1,50 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from '../Services/AuthServices/auth.service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { TokenDTO } from '../Interfaces/Auth/token-dto';
+import { RefreshTokenDTO } from '../Interfaces/Auth/refresh-token-dto';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const myToken = authService.getAccessToken();
+  console.log(myToken)
+  const router = inject(Router)
+
+  if(myToken){
+    req = req.clone({
+      setHeaders:({Authorization:`Bearer ${myToken}`})
+    })
+  }
+  return next(req).pipe(
+    catchError((err:any)=>{
+      if(err instanceof HttpErrorResponse){
+        if(err.status === 401){
+          let token:RefreshTokenDTO = {
+            accessToken:authService.getAccessToken()!,
+            refreshToken:authService.getRefreshToken()!
+          };
+
+          return authService.refreshToken(token)
+            .pipe(
+              switchMap((data:TokenDTO) => {
+                authService.storeToken(data);
+                req = req.clone({
+                  setHeaders:({Authorization:`Bearer ${data.accessToken}`})
+                })
+                return next(req)
+              }),
+              catchError((err) =>{
+                return throwError(()=>{
+                  alert("Token expired")
+                  router.navigate(['/login'])
+                })
+              })
+            )
+        }
+      }
+      return throwError(() => new Error("Nimadur xato ketdida"));
+    })
+  );
+};
