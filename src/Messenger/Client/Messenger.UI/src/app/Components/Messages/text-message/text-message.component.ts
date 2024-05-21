d9b0cd20805c670c4f4b324c1f0b62b0824f5e3d
Sardor <sardorstudent0618@gmail.com>
2024-05-21 15:12:32 +0500

    Text Message

---
+++ src/Messenger/Client/Messenger.UI/src/app/Components/Messages/text-message/text-message.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { MessageDTO } from '../../../Interfaces/Message/message-dto';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-text-message',
+  standalone: true,
+  imports: [DatePipe],
+  providers:[DatePipe],
+  templateUrl: './text-message.component.html',
+  styleUrl: './text-message.component.scss'
+})
+export class TextMessageComponent {
+  @Input() message!:MessageDTO;
+  @Input({required:true}) isMine!:boolean;
+
+  constructor(private datePipe: DatePipe) { }
+}
