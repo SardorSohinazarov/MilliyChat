d7100034745f9b48a087bfa1baaf4ebdb094150e
Sardor <sardorstudent0618@gmail.com>
2024-05-23 03:45:01 +0500

    Navbar edded

---
+++ src/Messenger/Client/Messenger.UI/src/app/Components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../Services/AuthServices/auth.service';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent{
+
+  constructor(private authService:AuthService,private router:Router){}
+
+  LogOut(){
+    this.authService.logOut();
+    this.router.navigate(['/auth/login'])
+  }
+}
