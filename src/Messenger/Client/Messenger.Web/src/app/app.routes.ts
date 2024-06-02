fb12f1a4433491daf09508b8c542234b2d1fc3f0
Sardor <sardorstudent0618@gmail.com>
2024-06-02 16:38:19 +0500

    Environment and Routes

--- src/Messenger/Client/Messenger.Web/src/app/app.routes.ts
+++ src/Messenger/Client/Messenger.Web/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './Components/Pages/not-found/not-found.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { ChatComponent } from './Components/chat/chat.component';
+import { LoginComponent } from './Components/Pages/Authentication/login/login.component';
+import { RegisterComponent } from './Components/Pages/Authentication/register/register.component';
 
-export const routes: Routes = [];
+const projectName = 'Milliy chat';
+
+export const routes: Routes = [
+    {path:'',redirectTo:'/chats',pathMatch:'full'},
+    {
+        path:'chats',
+        title:`Chats | ${projectName}`,
+        component:HomeComponent,
+        children:[
+            {path:':chatId',component:ChatComponent}
+        ]
+    },
+    {
+        path:'auth',
+        title:`Auth | ${projectName}`,
+        children:[
+            {path:'login',component:LoginComponent},
+            {path:'register',component:RegisterComponent}
+        ]
+    },
+    {path:'**',title:`not found | ${projectName}`, component:NotFoundComponent},
+];
